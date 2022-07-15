292328cfa98fd497a910f93755e567a186c543b7
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-07-15 08:00:42 +0100

    layers of the home page to comps

---
+++ Components/PageLayerOne.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PageLayerOne() {
+  return (
+    <div className='flex items-center justify-center w-full min-h-[200vh] h-fit bg-purple-500 mb-[100vh] z-[999]'>
+          <h1 className='text-white'>THIS IS THE FIRST LAYER</h1>
+    </div>
+  )
+}
---
+++ Components/PageLayerTwo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PageLayerTwo() {
+  return (
+    <div className='flex items-center justify-center fixed bottom-0 w-full h-screen bg-red-900'>
+          <h1>THIS IS THE SECOND LAYER</h1>
+    </div>
+  )
+}
