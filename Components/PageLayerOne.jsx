775b98c2696e299f660c555a24255accfde63ce7
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-07-15 08:07:30 +0100

    Site layout added

--- Components/PageLayerOne.jsx
+++ Components/PageLayerOne.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function PageLayerOne() {
   return (
-    <div className='flex items-center justify-center w-full min-h-[200vh] h-fit bg-purple-500 mb-[100vh] z-[999]'>
+    <div className='flex items-center justify-center w-full min-h-[200vh] h-fit bg-purple-700 mb-[100vh] z-[999]'>
           <h1 className='text-white'>THIS IS THE FIRST LAYER</h1>
     </div>
   )
--- Components/PageLayerTwo.jsx
+++ Components/PageLayerTwo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function PageLayerTwo() {
   return (
-    <div className='flex items-center justify-center fixed bottom-0 w-full h-screen bg-red-900'>
+    <div className='flex items-center justify-center fixed bottom-0 w-full h-screen bg-red-900 z-[-1]'>
           <h1>THIS IS THE SECOND LAYER</h1>
     </div>
   )
