2c4cb06aa5cfc0bd234c6a29f0111409f021d825
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-07-21 06:15:12 +0100

    Vercel Error Fix 1.0

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react'
-import style from '../styles/Home.module.css'
 
 export default function About() {
     const [state, setState] = useState(false)
   return (
-    <div className={style.container}>
+    <div>
         <h1>About Page</h1>
     </div>
   )
